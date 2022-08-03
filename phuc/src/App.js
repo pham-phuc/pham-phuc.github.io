d800b70b3de89fab03f3222208c488a5902e86ae
pham-phuc <101715209+pham-phuc@users.noreply.github.com>
2022-08-03 19:06:56 +0700

    js

--- phuc/src/App.js
+++ phuc/src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react"
 function App() {
-  return (
+  const [number, setNumber] = useState(1);
+  const [text, printfText] = useState("");
+  const incrementNumber = () => {
+    setNumber((number) => number + 1);
+  }
+  const decrementNumber = () => {
+    setNumber((number) => number - 1);
+  }
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <AppText text={text}/>
+     <AppText text={number}/>
+     <button onClick={incrementNumber}>IncrementNumber</button>
+     <button onClick={decrementNumber}>DecrementNumber</button>
+     
     </div>
   );
 }
-
+function AppText(props){
+  return <h1 class="AppText "> hello, {props.text}</h1>
+}
 export default App;
