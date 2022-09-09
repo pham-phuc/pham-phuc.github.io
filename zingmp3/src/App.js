b9059a2463fd77ec58e0e1a05a4dcb0cf5df8b0a
pham-phuc <101715209+pham-phuc@users.noreply.github.com>
2022-09-09 20:03:01 +0700

    push zingmp3

--- zingmp3/src/App.js
+++ zingmp3/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import ListSongs from "./components/Listsongs";
 import { Songs } from "./Context";
+import * as ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./components/Routes";
 import DataSongs from "./data/songs.json";
@@ -23,8 +24,7 @@ function App() {
         <div class="content-wrapper">
           <div class="content ">
             <Songs.Provider value={{ DataSongs, song, handleSetSong }}>
-              <div className="h-[633px] overflow-hidden overflow-y-scroll">
-                <ListSongs />
+              <div className="h-[633px] overflow-hidden overflow-y-scroll">               
                 <Routes />
               </div>
               <Playing />
