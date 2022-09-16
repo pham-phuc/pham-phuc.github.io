edd5467824510e3b6184d8a961c9290be93d428b
pham-phuc <101715209+pham-phuc@users.noreply.github.com>
2022-09-16 22:59:55 +0700

    creat app tiktok

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div> AC </div>
         <a
           className="App-link"
           href="https://reactjs.org"
