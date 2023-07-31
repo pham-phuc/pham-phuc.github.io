00bc771c66e68a3850a79af3aa2749e83ea5d83f
pham-phuc <101715209+pham-phuc@users.noreply.github.com>
2023-07-31 20:24:19 +0700

    Update main.js

--- clockrealtime/main.js
+++ clockrealtime/main.js
@@ -1,8 +1,8 @@
 function showTime() {
   var date = new Date();
-  var h = date.getHours(); 
-  var m = date.getMinutes(); 
-  var s = date.getSeconds(); 
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
   var session = "AM";
 
   if (h == 0) {
